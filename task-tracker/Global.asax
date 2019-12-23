fcfe8a4ffcd81c850f59bde55f3e0d357022c469
ViviL <47183724+aLeibowitz@users.noreply.github.com>
2019-12-23 22:40:14 +0200

    add asp.net mvc
    the regular template for mvc 5 with bootstrap

---
+++ task-tracker/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="task_tracker.MvcApplication" Language="C#" %>
---
+++ task-tracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(task_tracker.Startup))]
+namespace task_tracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
